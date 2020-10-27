23a233c6bc352b6a444c82c4d15de9ed414ee977
Judy Lipinski <Judy@karmoxie.com>
2020-10-27 19:31:41 -0400

    updated to use myCounter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = "My Angular Albums";
+  myCounter = 1;
+
+  ngOnInit(): void {
+    const interval = setInterval(() => this.myCounter++, 2000);
+
+    setTimeout(() => clearInterval(interval), 6000);
+  }
 }
