847137fb44287fc397c3087f3dbbc7a14bc8c00e
Judy Lipinski <Judy@karmoxie.com>
2020-10-27 22:33:55 -0400

    finished emit event

--- src/app/albums/album-card/album-card.component.ts
+++ src/app/albums/album-card/album-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Album } from '../album.model';
 
 @Component({
@@ -11,11 +11,14 @@ export class AlbumCardComponent implements OnInit {
   @Input()
   album: Album;
 
+  @Output()
+  albumClicked: EventEmitter<Album> = new EventEmitter<Album>();
+
   ngOnInit(): void {
   }
 
   showAlbum(): void {
-    alert('Album selected: ' + this.album.albumName);
+    this.albumClicked.emit(this.album);
   }
 
 }
