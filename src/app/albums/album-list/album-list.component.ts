c9907e46791173bba8f652bec6fb9ff71d8ca7f7
Judy Lipinski <Judy@karmoxie.com>
2020-10-28 00:08:59 -0400

    added import of service

--- src/app/albums/album-list/album-list.component.ts
+++ src/app/albums/album-list/album-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Album } from "../album.model";
 import { ALBUMS } from "../albums.data";
+import { AlbumService } from '../shared/album.service';
 
 @Component({
   selector: "app-album-list",
