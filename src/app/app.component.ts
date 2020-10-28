c0b251049b3d50e26a0a373082620617f2b55b85
Judy Lipinski <Judy@karmoxie.com>
2020-10-27 20:20:12 -0400

    added numbers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,15 @@ import { Component, OnInit } from "@angular/core";
 export class AppComponent implements OnInit {
   title = "My Angular Albums";
   myCounter = 1;
+  numbers: number[] = [1, 2, 3];
 
   ngOnInit(): void {
-    const interval = setInterval(() => this.myCounter++, 2000);
+    const interval = setInterval(() => {
+        this.myCounter++
+        this.numbers.push(4);
+        console.log(this.numbers);
+        this.numbers = [...this.numbers];
+    }, 2000);
 
     setTimeout(() => clearInterval(interval), 6000);
   }
